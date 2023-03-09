ad1bde3a281684a2590ac18daf674dcd95333735
ikaera <ikaera@gmail.com>
2023-03-08 21:48:15 -0500

    some loops.

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,44 +1,61 @@
+//
+//
+//
+
 const projects = [
   {
-    id: 'booze-hound',
-    techs: ['HTML', 'CSS', 'JS', 'API'],
+    id: "booze-hound",
+    techs: ["HTML", "CSS", "JS", "API"],
   },
   {
-    id: 'weather-dashboard',
-    techs: ['HTML', 'CSS', 'JS', 'API'],
+    id: "weather-dashboard",
+    techs: ["HTML", "CSS", "JS", "API"],
   },
 ];
-const techs = ['HTML', 'CSS', 'JS', 'API', 'Node', 'JQuery'];
-const btnContainerEl = document.querySelector('#tech-container');
-techs.forEach(tech => {
-  const btn = document.createElement('button');
-  btn.setAttribute('data-tech', tech);
+
+let renderProjects;
+
+const techs = ["HTML", "CSS", "JS", "API", "Node", "JQuery"];
+const btnContainerEl = document.querySelector("#tech-container");
+techs.forEach((tech) => {
+  const btn = document.createElement("button");
+  btn.setAttribute("data-tech", tech);
   btn.textContent = tech;
   btnContainerEl.appendChild(btn);
 });
 
-btnContainerEl.addEventListener('click', evt => {
+btnContainerEl.addEventListener("click", (evt) => {
   const btn = evt.target;
-  const selectedTech = btn.getAttribute('data-tech');
+  const selectedTech = btn.getAttribute("data-tech");
   console.log(selectedTech);
-  const projectsEl = document.querySelectorAll('.app');
-  projectsEl.forEach(project => {
-    const id = project.getAttribute('id');
-    const projectData = projects.find(proj => proj.id === id);
+  const projectsEl = document.querySelectorAll(".app");
+  projectsEl.forEach((project) => {
+    const id = project.getAttribute("id");
+    const projectData = projects.find((proj) => proj.id === id);
     console.log(projectData);
   });
+  renderProjects = projects.filter((project) => {
+    let exist = false;
+    project.techs.forEach((tech) => {
+      if (tech === selectedTech) {
+        exist = true;
+      }
+    });
+    return exist;
+  });
+  console.log(renderProjects);
 });
 
 // JQuery US Tooltip - custom styling
 $(function () {
   $(document).tooltip({
     position: {
-      my: 'center bottom-20',
-      at: 'center top',
+      my: "center bottom-20",
+      at: "center top",
       using: function (position, feedback) {
         $(this).css(position);
-        $('<div>')
-          .addClass('arrow')
+        $("<div>")
+          .addClass("arrow")
           .addClass(feedback.vertical)
           .addClass(feedback.horizontal)
           .appendTo(this);
