684e38aa3444bce3f36ec86bd06f5dd8b540aae5
ikaera <ikaera@gmail.com>
2023-03-09 04:29:39 -0500

    add search field to the work section.

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,42 +1,43 @@
+'use strict';
 //
 //
 //
 
 const projects = [
   {
-    id: "booze-hound",
-    techs: ["HTML", "CSS", "JS", "API"],
+    id: 'booze-hound',
+    techs: ['HTML', 'CSS', 'JS', 'API'],
   },
   {
-    id: "weather-dashboard",
-    techs: ["HTML", "CSS", "JS", "API"],
+    id: 'weather-dashboard',
+    techs: ['HTML', 'CSS', 'JS', 'API'],
   },
 ];
 
 let renderProjects;
 
-const techs = ["HTML", "CSS", "JS", "API", "Node", "JQuery"];
-const btnContainerEl = document.querySelector("#tech-container");
-techs.forEach((tech) => {
-  const btn = document.createElement("button");
-  btn.setAttribute("data-tech", tech);
+const techs = ['HTML', 'CSS', 'JS', 'API', 'Node', 'JQuery'];
+const btnContainerEl = document.querySelector('#tech-container');
+techs.forEach(tech => {
+  const btn = document.createElement('button');
+  btn.setAttribute('data-tech', tech);
   btn.textContent = tech;
   btnContainerEl.appendChild(btn);
 });
 
-btnContainerEl.addEventListener("click", (evt) => {
+btnContainerEl.addEventListener('click', evt => {
   const btn = evt.target;
-  const selectedTech = btn.getAttribute("data-tech");
+  const selectedTech = btn.getAttribute('data-tech');
   console.log(selectedTech);
-  const projectsEl = document.querySelectorAll(".app");
-  projectsEl.forEach((project) => {
-    const id = project.getAttribute("id");
-    const projectData = projects.find((proj) => proj.id === id);
+  const projectsEl = document.querySelectorAll('.app');
+  projectsEl.forEach(project => {
+    const id = project.getAttribute('id');
+    const projectData = projects.find(proj => proj.id === id);
     console.log(projectData);
   });
-  renderProjects = projects.filter((project) => {
+  renderProjects = projects.filter(project => {
     let exist = false;
-    project.techs.forEach((tech) => {
+    project.techs.forEach(tech => {
       if (tech === selectedTech) {
         exist = true;
       }
@@ -50,16 +51,26 @@ btnContainerEl.addEventListener("click", (evt) => {
 $(function () {
   $(document).tooltip({
     position: {
-      my: "center bottom-20",
-      at: "center top",
+      my: 'center bottom-20',
+      at: 'center top',
       using: function (position, feedback) {
         $(this).css(position);
-        $("<div>")
-          .addClass("arrow")
+        $('<div>')
+          .addClass('arrow')
           .addClass(feedback.vertical)
           .addClass(feedback.horizontal)
           .appendTo(this);
       },
     },
+  });
+});
+
+// jQuery - Filter
+$(document).ready(function () {
+  $('#myInput').on('keyup', function () {
+    var value = $(this).val().toLowerCase();
+    $('#apps *').filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
   });
 });
